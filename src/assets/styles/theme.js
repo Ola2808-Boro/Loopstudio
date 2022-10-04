ad7a1df1701c77bd897401abb731aadb6b330d2b
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-04 18:20:00 +0200

    add assets folder

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    white: "#ffffff",
+    black: "	#000000",
+    red: "#FF0000",
+    darkGray: "	#8c8c8c",
+    veryDarkGray: "#696969",
+    vividMulberry: "#C505EB",
+    lavenderIndigo: "#8647F8",
+  },
+  fontSizes: {
+    s: "15px",
+    m: "20px",
+    l: "25px",
+    xl: "30px",
+  },
+};

41874f7e80e8edc55e70edc51672c65214ea4678
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-04 18:57:17 +0200

    add Route and fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, useContext } from "react";
+import MainPage from "views/MainPage";
+import { dataContext } from "providers/DataProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import getData from "hooks/getData";
+function App() {
+  const { setData, data } = useContext(dataContext);
 
-const App = (props) => {
-  return <div></div>;
-};
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getData();
+      setData(data?.data);
+    }
+    fetchData();
+  }, []);
 
-App.propTypes = {};
+  return (
+    Object.keys(data).length !== 0 && (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    )
+  );
+}
 
 export default App;

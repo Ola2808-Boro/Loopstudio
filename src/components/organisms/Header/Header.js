5160d2e8baaebcf0f147f296bfa2165e78240f14
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-04 18:40:42 +0200

    create Header organism

---
+++ src/components/organisms/Header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import HeaderSection from "components/molecules/HeaderSection/HeaderSection";
+const Header = () => {
+  return <HeaderSection />;
+};
+
+export default Header;

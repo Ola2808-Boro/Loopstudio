72b528affedff6485577135086634751b9702a29
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-04 20:39:22 +0200

    create Prodycts view

---
+++ src/views/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductsSection from "components/organisms/ProductsSection/ProductsSection";
+import MainTemplates from "components/templates/MainTemplate/MainTemplates";
+const Products = () => {
+  return (
+    <MainTemplates>
+      <ProductsSection />
+    </MainTemplates>
+  );
+};
+
+export default Products;

8d1e3344e34cda8b2fba440de004d0c7679a5963
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-04 20:46:42 +0200

    create Contact view

---
+++ src/views/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "components/organisms/Form/Form";
+import MainTemplates from "components/templates/MainTemplate/MainTemplates";
+const Contact = () => {
+  return (
+    <MainTemplates>
+      <Form />
+    </MainTemplates>
+  );
+};
+
+export default Contact;
